899257e6cd86868f9af50d1c2b3a684b21834027
Ai. Kirill <aist.k.work@gmail.com>
2017-01-18 18:33:50 +0200

    1st

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("helo");
+
+
+var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+
+
+var getNewPhoto = function(data){
+  $(".bg").css("background")
+}
+var getNewQuote = function(data){
+  $(".quote-text").text(data.quoteText);
+    var quot = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' Author ' + data.quoteAuthor ;
+  if (data.quoteAuthor === ''); {
+    data.quoteAuthor = 'Unknown';
+  }
+
+  $(".author-text").text(' - ' + data.quoteAuthor  );
+  $(".twitter-share-button").attr("href", quot);
+
+};
+$(document).ready(function() {
+  $.getJSON(url, getNewQuote, 'jsonp');
+
+
+  $("#getNewQuote").click(function() {
+  $.getJSON(url, getNewQuote, 'jsonp');
+});
+
+});
+
+
+$(document).ready(function(){
+
+});
+
+
+
+console.log(document.documentElement.clientWidth);
+console.log(document.documentElement.clientHeight);
